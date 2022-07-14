e4cf2005560a20703d1055ccb0b392d770388456
suvmer <suvmer@mail.ru>
2022-07-14 16:42:38 +0300

    Homepage class

--- lib/main.dart
+++ lib/main.dart
@@ -16,32 +16,102 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-              child: Text(
-            "Привет, чем займемся сегодня?",
-            style: TextStyle(fontSize: 35),
-          )),
-          SizedBox(
-            height: 100,
-          ),
-          ElevatedButton(
-              child: Text("Создать новый конструктор"), onPressed: () {}),
-          SizedBox(
-            height: 40,
-          ),
-          ElevatedButton(child: Text("Посмотреть пример"), onPressed: () {}),
-          SizedBox(
-            height: 40,
-          ),
-          ElevatedButton(child: Text("Список конструкторов"), onPressed: () {}),
-        ],
-      ),
+      home: const MyHomePage(title: 'Главная'),
     );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+            child: Text(
+              "Привет, чем займемся сегодня?",
+              style: TextStyle(fontSize: 35),
+            )),
+        SizedBox(
+          height: 100,
+        ),
+        ElevatedButton(
+            child: Text("Создать новый конструктор"), onPressed: () {}),
+        SizedBox(
+          height: 40,
+        ),
+        ElevatedButton(child: Text("Посмотреть пример"), onPressed: () {}),
+        SizedBox(
+          height: 40,
+        ),
+        ElevatedButton(child: Text("Список конструкторов"), onPressed: () {}),
+      ],
+    );
+    /*Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );*/
   }
 }
 //16:28
