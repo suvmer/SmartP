96e5708c2005ed3052e6568b30c028cb3c9a0649
suvmer <suvmer@mail.ru>
2022-07-14 18:21:59 +0300

    headers

--- lib/main.dart
+++ lib/main.dart
@@ -63,47 +63,42 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Главная'),
+        ),
           body: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  TextButton(
-                      onPressed: () {Route route =
-                      MaterialPageRoute(builder: (context) => MyProj());
-                      Navigator.push(context, route);}, child: Text("Мои проекты")),
-                  SizedBox(width: 10),
-                  TextButton(
-                      onPressed: () {Route route =
-                      MaterialPageRoute(builder: (context) => Chernoviki());
-                      Navigator.push(context, route);}, child: Text("Черновики")),
-                ],
-              ),
-              SizedBox(
-                height: 100,
-              ),
-              ElevatedButton(
-                  child: Text("Создать новый конструктор"),
-                  onPressed: () {
-                    Route route =
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: Container(
+                child: Text(
+              "Привет, чем займемся сегодня?",
+              style: TextStyle(fontSize: 35),
+            )),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          ElevatedButton(
+              child: Text("Создать новый конструктор"),
+              onPressed: () {
+                Route route =
                     MaterialPageRoute(builder: (context) => NewConst());
-                    Navigator.push(context, route);
-                  }),
-              SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                  child: Text("Посмотреть пример"), onPressed: () {}),
-              SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                  child: Text("Список конструкторов"), onPressed: () {}),
-            ],
-          )),
+                Navigator.push(context, route);
+              }),
+          SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(child: Text("Посмотреть пример"), onPressed: () {}),
+          SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(child: Text("Список конструкторов"), onPressed: () {}),
+        ],
+      )),
     );
     /*Scaffold(
       appBar: AppBar(
@@ -139,35 +134,16 @@ class NewConst extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Новый конструктор'),
+          ),
           body: Center(
-            child: TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Back"),
-            ),
+            child: TextButton(onPressed: () {
+              Navigator.pop(context);
+            }, child: Text("Back"),),
           )),
     );
   }
 }
 
-class MyProj extends StatelessWidget {
-  const MyProj({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class Chernoviki extends StatelessWidget {
-  const Chernoviki({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-
-//18:15
+//17:42
